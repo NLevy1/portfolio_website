04ca713d4c877231ec8f0fde5db893ce26366742
NLevy1 <nathanaellevy@hotmail.com>
2022-06-23 01:14:13 +0200

    Add projects: portfolio website and Change return program. Update content.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from sheets import DataManager
-import requests
 
 app = Flask(__name__)
 
@@ -13,15 +12,10 @@ def get_main_page():
     return render_template("index.html", projects=dict_list)
 
 
-@app.route("/project/<id>")
-def get_project(id):
+@app.route("/project/<project_id>")
+def get_project(project_id):
     return render_template("project.html",
-                           id=str(id),
-                           projects=dict_list)
-
-@app.route("/all_projects")
-def get_all_projects():
-    return render_template("all_projects.html",
+                           id=str(project_id),
                            projects=dict_list)
 
 
